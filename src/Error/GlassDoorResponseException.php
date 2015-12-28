c6b896d43516aa38ccc877be60d1c2da2d247ec2
Karl Hughes <khughes.me@gmail.com>
2015-12-27 19:04:03 -0500

    More doc blocks

--- src/Error/GlassDoorResponseException.php
+++ src/Error/GlassDoorResponseException.php
@@ -2,11 +2,13 @@
 
 /**
  * @file
+ * Glassdoor Response Exception.
  */
 
 namespace Glassdoor\Error;
+
 /**
- *
+ * GlassDoorResponseException class.
  */
 class GlassDoorResponseException extends GlassdoorException {
 
